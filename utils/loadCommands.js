463d6b9db7c88ea0dd9d1c54a2a86e30fce9457f
And4ewMG <and4ewmg@gmail.com>
2024-06-08 12:50:10 +0100

    Update loadCommands.js

--- utils/loadCommands.js
+++ utils/loadCommands.js
@@ -1,1 +1,16 @@
+const fs = require('node:fs');
+const path = require('node:path');
 
+const loadCommands = (commandFiles, commandsPath, commandCollection) => {
+    commandFiles.forEach((file) => {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if (command.data && command.execute) {
+            commandCollection.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    });
+};
+
+module.exports = { loadCommands };
